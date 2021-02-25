494e68594076ae54379f83d9420b852a09916bd3
Florian <florian.lamotte@gmail.com>
2021-02-25 12:52:36 +0000

    Added base project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-flutter-words-list-manager
+# flutter_words_list_manager
+
+Simple Flutter words list manager.
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_words_list_manager/RandomWords.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Words List Manager",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Words List Manager")
+        ),
+        body: Center(
+          child: RandomWords(),
+        ),
+      )
+    );
+  }
+
+}
